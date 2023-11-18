1e17fc88c16135f959d7d565995a6137e86077f8
Francisco Soto <sotofloresfrancisco@gmail.com>
2023-11-17 19:45:28 -0500

    eliminando archivos

--- js/toggle-logo.js
+++
@@ -1,8 +0,0 @@
-/* document.addEventListener("DOMContentLoaded", () => {
-  const analytikaCore = document.querySelector('.analytikaCore');
-
-  window.addEventListener('scroll', () => {
-    analytikaCore.classList.toggle('d-none', window.scrollY > 50);
-  });
-});
- */
