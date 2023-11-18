4e180d8bfc413cf043fdf86cd5716a4b0feadf65
Francisco Soto <sotofloresfrancisco@gmail.com>
2023-11-17 19:39:09 -0500

    agregando archivos js

--- js/toggle-logo.js
+++ js/toggle-logo.js
@@ -1,7 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
+/* document.addEventListener("DOMContentLoaded", () => {
   const analytikaCore = document.querySelector('.analytikaCore');
 
   window.addEventListener('scroll', () => {
     analytikaCore.classList.toggle('d-none', window.scrollY > 50);
   });
 });
+ */
